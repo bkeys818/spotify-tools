955d98cacaf86422a915dfe30a8b8b32ab51c360
Ben Keys <bkeys818@gmail.com>
2022-07-05 14:09:31 -0400

    💄 Create a basic header

---
+++ src/lib/header.svelte
@@ -0,0 +1,17 @@
+<nav class="bg-gray-800">
+	<div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+		<div class="relative flex items-center justify-between h-16">
+			<div
+				class="absolute inset-y-0 left-0 flex items-center pr-2 sm:static sm:inset-auto sm:mr-6 sm:pl-0"
+			>
+				<p class="text-2xl font-extrabold tracking-tight text-white">Spotify Tools</p>
+			</div>
+			<div class="absolute inset-y-0 right-0 flex items-center">
+				<a
+					class="text-lg font-medium text-gray-100 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md"
+					href="./">Log In</a
+				>
+			</div>
+		</div>
+	</div>
+</nav>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,8 @@
 <script>
 	import '../app.css';
+	import Header from '$lib/header.svelte';
 </script>
+
+<Header />
 
 <slot />
