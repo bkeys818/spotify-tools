166058a3c089e792dfc178d274b259ca10cf74db
Ben Keys <bkeys818@gmail.com>
2022-07-08 10:29:15 -0400

    🚧 Create Spotify authorization methods

---
+++ src/lib/spotify/authorize.ts
@@ -0,0 +1,39 @@
+import { credential } from '.';
+
+export function createAuthorizeURL(scopes: string[]) {
+	location.href =
+		'https://accounts.spotify.com/authorize?' +
+		new URLSearchParams({
+			response_type: 'code',
+			client_id: credential.clientId,
+			scope: scopes.join(' '),
+			redirect_uri: credential.redirectUri,
+			// state: getState()
+		}).toString();
+}
+
+export async function getAccessToken(code: string) {
+	// check state
+	const { VITE_CLIENT_SECRET } = import.meta.env;
+	const response = await fetch('https://accounts.spotify.com/api/token', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			Authorization:
+				'Basic ' + Buffer.from(credential.clientId + ':' + VITE_CLIENT_SECRET).toString('base64')
+		},
+		body: [
+			['grant_type', 'authorization_code'],
+			['code', code],
+			['redirect_uri', credential.redirectUri]
+		]
+			.map((a) => a.join('='))
+			.join('&')
+	});
+
+	if (response.ok) {
+		return await response.json();
+	} else {
+		throw new Error(`Failed to authorize Spotify! Status text: '${response.statusText}'.`);
+	}
+}
