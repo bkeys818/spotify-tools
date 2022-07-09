e41c839fc5d997a0ed7c92e8648f17d52f80a686
Ben Keys <bkeys818@gmail.com>
2022-07-09 09:59:35 -0400

    🚚 Rename 'modules' to 'tools'

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,4 @@
 <script context="module" lang="ts">
     import { base } from '$app/paths';
 </script>
-<a href={`.${base}/modules/public-liked-songs-playlist`}>Pulbic "Liked Songs" Playlist</a>
+<a href={`.${base}/tools/public-liked-songs-playlist`}>Pulbic "Liked Songs" Playlist</a>
