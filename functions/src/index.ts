5a9b6a29938a5d330e040c95d1be7149cd194a79
Ben Keys <bkeys818@gmail.com>
2022-07-13 15:28:34 -0400

    ✨ Create updated public liked songs function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,13 @@
-import * as functions from "firebase-functions";
+import * as admin from 'firebase-admin';
+admin.initializeApp();
+import * as functions from 'firebase-functions';
+import { db } from './global';
+import { updatePublicLikedSongs } from './public-liked-songs';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const updatePublicLikedSongsPlaylists = functions
+	.runWith({ secrets: ['SPOTIFY_CLIENT_ID', 'SPOTIFY_CLIENT_SECRET'] })
+	.pubsub.schedule('0 0 * * *')
+	.onRun(async () => {
+		const docRefs = await db.collection('public-liked-songs').listDocuments();
+		return await Promise.all(docRefs.map(updatePublicLikedSongs));
+	});
---
+++ functions/src/public-liked-songs.ts
@@ -0,0 +1,77 @@
+import { handleResponse } from './global';
+import SpotifyWebApi from 'spotify-web-api-node';
+
+export async function updatePublicLikedSongs(
+	ref: FirebaseFirestore.DocumentReference<FirebaseFirestore.DocumentData>
+) {
+	// get data from document
+	const doc = await ref.get();
+	const data = doc.data();
+	if (!data) return;
+	const { playlist_id, refresh_token } = data;
+
+	// authorize
+	const spotify = new SpotifyWebApi({
+		clientId: process.env.SPOTIFY_CLIENT_ID,
+		clientSecret: process.env.SPOTIFY_CLIENT_SECRET
+	});
+	spotify.setRefreshToken(refresh_token);
+	const { access_token } = await handleResponse(() => spotify.refreshAccessToken());
+	spotify.setAccessToken(access_token);
+
+	const [playlistTracks, savedTracks] = await Promise.all([
+		getPlaylistTracks(spotify, playlist_id),
+		getSavedTracks(spotify)
+	]);
+
+	const removedTracks = playlistTracks.filter(
+		(track) => !savedTracks.some((savedTrack) => savedTrack.id === track.id)
+	);
+	const addedTracks = savedTracks.filter(
+		(track) => !playlistTracks.some((playlistTrack) => playlistTrack.id === track.id)
+	);
+
+	const updateMethods: Parameters<typeof handleResponse>[0][] = [];
+	if (removedTracks.length > 0)
+		updateMethods.push(() => spotify.removeTracksFromPlaylist(playlist_id, removedTracks));
+	if (addedTracks.length > 0)
+		updateMethods.push(() =>
+			spotify.addTracksToPlaylist(
+				playlist_id,
+				addedTracks.map((track) => track.uri)
+			)
+		);
+
+	await Promise.all(updateMethods.map(handleResponse));
+}
+
+async function getPlaylistTracks(spotify: SpotifyWebApi, playlistId: string) {
+	const items: SpotifyApi.PlaylistTrackObject[] = [];
+	let total = 1;
+	while (items.length < total) {
+		const response = await handleResponse(() =>
+			spotify.getPlaylistTracks(playlistId, {
+				limit: 50,
+				offset: items.length
+			})
+		);
+		total = response.total;
+		items.push(...response.items);
+	}
+	return items
+		.map((item) => item.track)
+		.filter((item): item is SpotifyApi.TrackObjectFull => Boolean(item));
+}
+
+async function getSavedTracks(spotify: SpotifyWebApi) {
+	const items: SpotifyApi.SavedTrackObject[] = [];
+	let total = 1;
+	while (items.length < total) {
+		const response = await handleResponse(() =>
+			spotify.getMySavedTracks({ limit: 50, offset: items.length })
+		);
+		total = response.total;
+		items.push(...response.items);
+	}
+	return items.map((item) => item.track);
+}
