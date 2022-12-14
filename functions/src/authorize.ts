96ffccbad10914fb5e8574019d83a71473043055
Ben Keys <bkeys818@gmail.com>
2022-12-14 12:33:41 -0500

    ✨ Create authorizeTool function

---
+++ functions/src/authorize.ts
@@ -0,0 +1,18 @@
+import { handleResponse } from './global';
+import SpotifyWebApi from 'spotify-web-api-node';
+import { db } from './global';
+
+export async function authorizeTool(tool: string, code: string) {
+	const spotify = new SpotifyWebApi({
+		clientId: process.env.SPOTIFY_CLIENT_ID,
+		clientSecret: process.env.SPOTIFY_CLIENT_SECRET
+	});
+    spotify.setRedirectURI(process.env.SPOTIFY_REDIRECT_URI as string)
+	const { refresh_token, access_token } = await handleResponse(() => spotify.authorizationCodeGrant(code));
+	spotify.setAccessToken(access_token)
+    const { id } = await handleResponse(() => spotify.getMe())
+	const ref = db.doc(tool + '/' + id);
+	const doc = await ref.get();
+	if (doc.exists) await ref.update({ refresh_token });
+	else await ref.create({ refresh_token });
+}
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,14 @@ import * as admin from 'firebase-admin';
 admin.initializeApp();
 import * as functions from 'firebase-functions';
 import { db } from './global';
+import * as authorize from './authorize'
 import { updatePublicLikedSongs } from './public-liked-songs';
+
+export const authorizeTool = functions
+	.runWith({ secrets: ['SPOTIFY_CLIENT_ID', 'SPOTIFY_CLIENT_SECRET'] })
+	.https.onCall(async (data: { tool: string, code: string }) => {
+		return await authorize.authorizeTool(data.tool, data.code)
+	})
 
 export const updatePublicLikedSongsPlaylists = functions
 	.runWith({ secrets: ['SPOTIFY_CLIENT_ID', 'SPOTIFY_CLIENT_SECRET'] })
