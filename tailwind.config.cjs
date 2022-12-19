a8601087e7c7e0aa87c1502d4822cbe9ade25053
Ben Keys <bkeys818@gmail.com>
2022-12-19 14:18:24 -0500

    🔧 Fix tailwind configuration

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
+  safelist: [
+    ...new Array(10).fill('').map((_, i) => `h-${i*2+12}`),
+    ...new Array(6).fill('').map((_, i) => `border-${i*2+8}`)
+  ],
   theme: {
     extend: {},
   },
