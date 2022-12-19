84acce37c66837baa944c14aa2614ac658c1c683
Ben Keys <bkeys818@gmail.com>
2022-12-19 14:25:29 -0500

    ⚰️ Clean up tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
-  safelist: [
-    ...new Array(10).fill('').map((_, i) => `h-${i*2+12}`),
-    ...new Array(6).fill('').map((_, i) => `border-${i*2+8}`)
-  ],
   theme: {
     extend: {},
   },
