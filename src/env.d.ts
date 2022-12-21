1dbc4fb4c3ad8d41b57e52802e7aab1afcc19455
Ben Keys <bkeys818@gmail.com>
2022-12-21 14:33:44 -0500

    🏷️ Update .env types

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-	readonly PUBLIC_ORIGIN: string
 	readonly PUBLIC_CLIENT_ID: string
-	readonly CLIENT_SECRET: string
 }
 
 interface ImportMeta {
