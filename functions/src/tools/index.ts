feae4e8460e0240b7f3c050d0845f9fbf531ea65
Ben Keys <bkeys818@gmail.com>
2022-12-22 23:16:55 -0500

    ✨ Remove delete playlist_ids

--- functions/src/tools/index.ts
+++ functions/src/tools/index.ts
@@ -6,7 +6,16 @@ export async function createPublicLikedSongs(
 	const doc = await ref.get()
 	const data = doc.data() as publicLikedSongs.Data
 
-	if (!data.playlist_id) data.playlist_id = await publicLikedSongs.create(data.refresh_token)
-	ref.update({ playlist_id: data.playlist_id })
-	return await publicLikedSongs.update(data.refresh_token, data.playlist_id)
+	if (!data.playlist_id) {
+		data.playlist_id = await publicLikedSongs.create(data.refresh_token)
+		await ref.update({ playlist_id: data.playlist_id })
+	}
+	try {
+		return await publicLikedSongs.update(data.refresh_token, data.playlist_id)
+	} catch (error) {
+		if (typeof error == 'object' && error && 'statusCode' in error)
+			if ((error as any).statusCode == 404)
+				return await ref.update({ playlist_id: undefined })
+		throw error
+	}
 }
