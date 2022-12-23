139142e6c5210e4110380f9d4e34b0e3fcc18102
Ben Keys <bkeys818@gmail.com>
2022-12-23 10:55:26 -0500

    🎨 Use URLSearchParams to decode url query

--- src/routes/authorize/+page.svelte
+++ src/routes/authorize/+page.svelte
@@ -5,26 +5,16 @@
 	import { error } from '@sveltejs/kit'
 
 	onMount(async () => {
-		const searchParams = getSearchParams()
+		const searchParams = new URLSearchParams(location.search.slice(1))
 		// check scope
-		if (searchParams.code) {
+		if (searchParams.has('code')) {
 			const toolPath = directedFromPath.parse(document.cookie)
 			if (toolPath) {
 				location.href = toolPath + location.search
 			} else throw error(404, `I'm lost! Where did you come from?`)
 		} else {
-			const scopes: string[] = (searchParams.scopes ?? '').split(' ')
+			const scopes: string[] = (searchParams.get('scopes') ?? '').split(' ')
 			location.href = createAuthorizeURL(scopes, location.origin)
 		}
 	})
-	function getSearchParams() {
-		return location.search
-			.slice(1)
-			.split('&')
-			.map(v => v.split('='))
-			.reduce(
-				(pre: Partial<Record<string, string>>, [key, value]) => ({ ...pre, [key]: value }),
-				{}
-			)
-	}
 </script>
