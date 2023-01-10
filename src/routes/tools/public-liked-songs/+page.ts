eebe844c8659a72390c35fae2cfcd1bcede84731
Ben Keys <bkeys818@gmail.com>
2023-01-10 11:24:08 -0500

    🎨 Use 'satisfies' from TS 4.9

--- src/routes/tools/public-liked-songs/+page.ts
+++ src/routes/tools/public-liked-songs/+page.ts
@@ -1,7 +1,7 @@
 import type { PageLoad } from './$types'
 
-export const load: PageLoad = () => ({
+export const load = () => ({
 	title: 'Public "Liked Songs" Playlist',
 	desc: 'Creates a public paylist that contains all your liked songs. Then updates that playlist every day.\nThis allows your people who follow you to see and play your liked songs.',
 	scopes: ['user-library-read', 'playlist-modify-public']
-})
+}) satisfies PageLoad
