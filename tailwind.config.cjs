e43254a60e2bcf21d7b4fc4a64ec6acc51797c52
Ben Keys <bkeys818@gmail.com>
2023-01-15 11:43:12 -0500

    🔧 Add Spotify colors to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
-	},
-	plugins: []
+		extend: {
+			colors: {
+				'spotify-green': {
+					400: '#1ED760',
+					500: '#1DB954'
+				},
+				'spotify-white': '#FFFFFF',
+				'spotify-black': '#191414',
+				'spotify-gray': {
+					200: '#C1C3C6',
+					600: '#616467',
+				}
+			}
+		},
+	}
 }
