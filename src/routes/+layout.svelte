76668d217172dcfef4bb371f42b54a9aa70a2d46
Ben Keys <bkeys818@gmail.com>
2023-01-16 10:22:58 -0500

    🚚 Rename header to NavBar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,9 @@
 <script>
 	import '../app.css'
-	import Header from '$lib/components/header.svelte'
+	import NavBar from '$lib/components/NavBar.svelte'
 </script>
 
-<Header />
+<NavBar />
 
 <main class='max-w-4xl mx-auto px-4 my-4 sm:my-2 lg:my-8'>
 	<slot />
