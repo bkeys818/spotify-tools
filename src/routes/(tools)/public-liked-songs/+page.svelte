02202fe81089ac5728f72111e9ef434073600371
Ben Keys <bkeys818@gmail.com>
2023-01-17 17:46:18 -0500

    🚚 Create AuthorizeButton and LoginButton

--- src/routes/(tools)/public-liked-songs/+page.svelte
+++ src/routes/(tools)/public-liked-songs/+page.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
-	import type { PageData } from './$types'
-	import { authorize } from '$lib/spotify'
-	import { setCookie } from '$lib/cookie'
 	import { onMount } from 'svelte'
 	import { createPublicLikedSongs } from '$lib/firebase/functions'
 	import { user } from '$lib/stores'
+	import AuthorizeButton from '$lib/components/AuthorizeButton.svelte'
+	import LoginButton from '$lib/components/LoginButton.svelte'
 	import Spinner from '$lib/components/spinner.svelte'
 	import SpotifyEmbed from '$lib/components/spotify-embed.svelte'
 
-	export let data: PageData
 	let backendResponse: ReturnType<typeof createPublicLikedSongs> | undefined
 
 	onMount(async () => {
@@ -39,19 +37,8 @@
 			<p>{error}</p>
 		{/await}
 	{:else if $user}
-		<!-- Spotify Logo -->
-		<button
-			class="btn-primary"
-			on:click={() => {
-				setCookie('directed_from_path', location.pathname)
-				authorize('user-library-read playlist-modify-public')
-			}}>Authorize</button
-		>
-		<p class="mt-4">
-			In order to use our tools, we need limited access to your Spotify account.
-		</p>
+		<AuthorizeButton />
 	{:else}
-		<a href={data.loginUrl} class="btn-primary">Login</a>
-		<p>Log in to use our tools</p>
+		<LoginButton />
 	{/if}
 </div>
