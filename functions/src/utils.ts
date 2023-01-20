8adad749fb213218b481dbdb94b223b3f7bc40b8
Ben Keys <bkeys818@gmail.com>
2023-01-20 13:05:58 -0500

    🎨 Format code

--- functions/src/utils.ts
+++ functions/src/utils.ts
@@ -18,7 +18,11 @@ interface Response<T> {
 	statusCode: number
 }
 
-export async function forEvery<T>(items: T[], limit: number, method: (items: T[]) => Promise<unknown>) {
+export async function forEvery<T>(
+	items: T[],
+	limit: number,
+	method: (items: T[]) => Promise<unknown>
+) {
 	for (let i = 0; i < items.length; i += limit) {
 		await method(items.slice(i, i + limit))
 	}
