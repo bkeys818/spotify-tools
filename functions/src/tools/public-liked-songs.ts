bebc7fa838b603d73e8c791ba0d5cdb2255002cf
Ben Keys <bkeys818@gmail.com>
2023-07-13 07:43:04 -0400

    🚧 Implement firebase-function 2nd gen

--- functions/src/tools/public-liked-songs.ts
+++ functions/src/tools/public-liked-songs.ts
@@ -1,4 +1,6 @@
-import * as functions from 'firebase-functions'
+import { onCall, HttpsError } from 'firebase-functions/v2/https'
+import { onSchedule } from 'firebase-functions/v2/scheduler'
+import { warn } from 'firebase-functions/logger'
 import { db } from '../firestore'
 import { Spotify } from '../spotify'
 import { forEvery } from '../utils'
@@ -9,101 +11,98 @@ interface Document {
 	playlist_id?: string
 }
 
-export const createPublicLikedSongs = functions
-	.runWith({ secrets })
-	.https.onCall(async (data: Data, context) => {
-		if (!context.auth)
-			throw new functions.https.HttpsError('unauthenticated', 'User must be authenticated.')
+export const createPublicLikedSongs = onCall<Data>({ secrets }, async ({ data, auth }) => {
+	if (!auth)
+		throw new HttpsError('unauthenticated', 'User must be authenticated.')
 
-		const spotify = new Spotify({
-			clientId: process.env.SPOTIFY_CLIENT_ID,
-			clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-			redirectUri: data.origin + '/authorize'
-		})
-		const { refresh_token } = await spotify.authorizationCodeGrant(data.code)
-		const user = await spotify.getMe()
-
-		const ref = db.collection('public-liked-songs').doc(user.id)
-		let doc = await ref.get()
-
-		let docData: Document
-		if (doc.exists) {
-			docData = doc.data() as Document
-		} else {
-			await ref.create({ refresh_token, uid: context.auth.uid })
-			doc = await ref.get() // Do I need this line?
-			docData = doc.data() as Document
-		}
-
-		// Don't have playlist id
-		if (!docData.playlist_id) {
-			// check to see if playlist already exists
-			const playlists = await spotify.getMyPlaylists()
-			const playlistName = name(user.display_name)
-			for (const playlist of playlists) {
-				if (playlist.name == playlistName) docData.playlist_id = playlist.id
-			}
-			// if playlist doesn't exist
-			if (!docData.playlist_id) {
-				const playlist = await spotify.createPlaylist(user.id, {
-					name: playlistName,
-					description: description(),
-					public: true
-				})
-				docData.playlist_id = playlist.id
-			}
-			await ref.update({ playlist_id: docData.playlist_id })
-		}
-
-		if (!(await spotify.usersFollowPlaylist(docData.playlist_id, [user.id]))[0]) {
-			await ref.delete()
-			throw new functions.https.HttpsError(
-				'not-found',
-				'You may have deleted the synced playlist. Refresh to restore it.'
-			)
-		}
-
-		try {
-			return await update(spotify, docData.playlist_id)
-		} catch (error) {
-			let msg = 'Spotify Error'
-			functions.logger.warn(error)
-			if (typeof error == 'object' && error && 'statusCode' in error) {
-				if ('message' in error && typeof error.message == 'string') msg = error.message
-				throw new functions.https.HttpsError('unknown', msg, error)
-			}
-			throw error
-		}
+	const spotify = new Spotify({
+		clientId: process.env.SPOTIFY_CLIENT_ID,
+		clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+		redirectUri: data.origin + '/authorize'
 	})
+	const { refresh_token } = await spotify.authorizationCodeGrant(data.code)
+	const user = await spotify.getMe()
+
+	const ref = db.collection('public-liked-songs').doc(user.id)
+	let doc = await ref.get()
+
+	let docData: Document
+	if (doc.exists) {
+		docData = doc.data() as Document
+	} else {
+		await ref.create({ refresh_token, uid: auth.uid })
+		doc = await ref.get() // Do I need this line?
+		docData = doc.data() as Document
+	}
+
+	// Don't have playlist id
+	if (!docData.playlist_id) {
+		// check to see if playlist already exists
+		const playlists = await spotify.getMyPlaylists()
+		const playlistName = name(user.display_name)
+		for (const playlist of playlists) {
+			if (playlist.name == playlistName) docData.playlist_id = playlist.id
+		}
+		// if playlist doesn't exist
+		if (!docData.playlist_id) {
+			const playlist = await spotify.createPlaylist(user.id, {
+				name: playlistName,
+				description: description(),
+				public: true
+			})
+			docData.playlist_id = playlist.id
+		}
+		await ref.update({ playlist_id: docData.playlist_id })
+	}
+
+	if (!(await spotify.usersFollowPlaylist(docData.playlist_id, [user.id]))[0]) {
+		await ref.delete()
+		throw new HttpsError(
+			'not-found',
+			'You may have deleted the synced playlist. Refresh to restore it.'
+		)
+	}
+
+	try {
+		return await update(spotify, docData.playlist_id)
+	} catch (error) {
+		let msg = 'Spotify Error'
+		warn(error)
+		if (typeof error == 'object' && error && 'statusCode' in error) {
+			if ('message' in error && typeof error.message == 'string') msg = error.message
+			throw new HttpsError('unknown', msg, error)
+		}
+		throw error
+	}
+})
+
 interface Data {
 	code: string
 	origin: string
 }
 
-export const syncPublicLikedSongs = functions
-	.runWith({ secrets })
-	.pubsub.schedule('0 0 * * *')
-	.onRun(async () => {
-		const docRefs = await db.collection('public-liked-songs').listDocuments()
-		return await Promise.all(
-			docRefs.map(async ref => {
-				const doc = await ref.get()
-				const data = doc.data() as Document
-				const spotify = new Spotify({
-					clientId: process.env.SPOTIFY_CLIENT_ID,
-					clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-					refreshToken: data.refresh_token
-				})
-				await spotify.refreshAccessToken()
-				if (data.playlist_id) {
-					const user = await spotify.getMe()
-					if (!(await spotify.usersFollowPlaylist(data.playlist_id, [user.id]))[0])
-						return await ref.delete()
-					else return await update(spotify, data.playlist_id)
-				} else return await ref.delete()
+export const syncPublicLikedSongs = onSchedule({ schedule: '0 0 * * *', secrets }, async () => {
+	const docRefs = await db.collection('public-liked-songs').listDocuments()
+	await Promise.all(
+		docRefs.map(async ref => {
+			const doc = await ref.get()
+			const data = doc.data() as Document
+			const spotify = new Spotify({
+				clientId: process.env.SPOTIFY_CLIENT_ID,
+				clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+				refreshToken: data.refresh_token
 			})
-		)
-	})
+			await spotify.refreshAccessToken()
+			if (data.playlist_id) {
+				const user = await spotify.getMe()
+				if (!(await spotify.usersFollowPlaylist(data.playlist_id, [user.id]))[0])
+					return await ref.delete()
+				else return await update(spotify, data.playlist_id)
+			} else return await ref.delete()
+		})
+	)
+	return
+})
 
 async function update(spotify: Spotify, playlistId: string) {
 	const [playlistTracks, savedTracks] = await Promise.all([
