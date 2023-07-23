48ea072857a27f7b850cd125c071de6f45a3dd35
Ben Keys <bkeys818@gmail.com>
2023-07-23 17:25:04 -0400

    ➕ Replace eslint-plugin-svelte3 with eslint-plugin-svelte

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,31 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'prettier',
+		'plugin:svelte/recommended'
+	],
+	plugins: ['@typescript-eslint'],
 	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	settings: {
-		'svelte3/typescript': () => require('typescript')
-	},
 	parserOptions: {
 		sourceType: 'module',
-		ecmaVersion: 2020
+		ecmaVersion: 2020,
+		project: [
+			'./tsconfig.json',
+			'./functions/src/tsconfig.json',
+			'./functions/tests/tsconfig.json'
+		],
+		extraFileExtensions: ['.svelte']
 	},
+	overrides: [
+		{
+			files: ['*.svelte'],
+			parser: 'svelte-eslint-parser',
+			parserOptions: { parser: '@typescript-eslint/parser' }
+		}
+	],
 	env: {
 		browser: true,
 		es2017: true,
