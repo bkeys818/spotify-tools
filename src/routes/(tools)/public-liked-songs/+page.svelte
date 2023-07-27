5ee52149f466c4be9a16ba7c7f679d257d0d8190
Ben Keys <bkeys818@gmail.com>
2023-07-26 21:56:37 -0400

    🏗️ Split Firebase and Spotify authorization

--- src/routes/(tools)/public-liked-songs/+page.svelte
+++ src/routes/(tools)/public-liked-songs/+page.svelte
@@ -2,19 +2,22 @@
 	import { onMount } from 'svelte'
 	import { publicLikedSongs } from '$lib/firebase/functions'
 	import { parseCode } from '$lib/spotify'
-	import Authorize from '$lib/components/Authorize.svelte'
+	import AuthFirebase from '$lib/components/AuthFirebase.svelte'
+	import AuthSpotifyButton from '$lib/components/AuthSpotifyButton.svelte'
 	import Spinner from '$lib/components/spinner.svelte'
 	import SpotifyEmbed from '$lib/components/spotify-embed.svelte'
 	import ErrorMsg from '$lib/components/ErrorMsg.svelte'
 
-	let playlistId: Promise<string>
+	let playlistId: Promise<string> | undefined
 	let isPopulated: boolean | undefined = false
 	let error: unknown
 
 	onMount(() => {
 		try {
 			const code = parseCode()
-			if (code) { playlistId = create(code) }
+			if (code) {
+				playlistId = create(code)
+			}
 		} catch (err) {
 			error = err
 		}
@@ -38,26 +41,30 @@
 </script>
 
 <div class="my-4 text-center">
-	<Authorize firebase spotify={playlistId === undefined}>
-		{#await playlistId}
-			<Spinner />
-		{:then playlistId}
-			<SpotifyEmbed
-				title="public-liked-songs"
-				type="playlist"
-				id={playlistId}
-				className="mx-auto"
-			/>
-			{#if isPopulated === true}
-				<p>Playlist synced!</p>
-			{:else if isPopulated === false}
-				<p class="loading">Populating playlist</p>
+	<AuthFirebase>
+		{#if !playlistId}
+			<AuthSpotifyButton authType="code" scopes="user-library-read playlist-modify-public" />
+		{:else}
+			{#await playlistId}
+				<Spinner />
+			{:then playlistId}
+				<SpotifyEmbed
+					title="public-liked-songs"
+					type="playlist"
+					id={playlistId}
+					className="mx-auto"
+				/>
+				{#if isPopulated === true}
+					<p>Playlist synced!</p>
+				{:else if isPopulated === false}
+					<p class="loading">Populating playlist</p>
+				{/if}
+			{:catch error}
+				<ErrorMsg {error} />
+			{/await}
+			{#if error}
+				<ErrorMsg {error} />
 			{/if}
-		{:catch error}
-			<ErrorMsg {error} />
-		{/await}
-		{#if error}
-			<ErrorMsg {error} />
 		{/if}
-	</Authorize>
+	</AuthFirebase>
 </div>
