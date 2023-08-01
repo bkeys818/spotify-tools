53afbf3f180138b8b203dcf8f9e548c1b2fa9b76
Ben Keys <bkeys818@gmail.com>
2023-08-01 16:47:52 -0400

    🏷️ Export FilteredTrack type

--- src/lib/spotify/index.ts
+++ src/lib/spotify/index.ts
@@ -8,20 +8,25 @@ export function getMyPlaylists(token: string) {
 	return getAll<SpotifyApi.PlaylistObjectSimplified>('me/playlists', token)
 }
 
+type FilteredTrack = Pick<
+	SpotifyApi.TrackObjectFull,
+	'id' | 'name' | 'is_local' | 'duration_ms' | 'type'
+> & {
+	album: Pick<SpotifyApi.TrackObjectFull['album'], 'id' | 'name' | 'images'>
+	artists: Pick<SpotifyApi.TrackObjectFull['album']['artists'][number], 'id' | 'name'>[]
+}
+export type TrackObj = FilteredTrack & { index: number }
 export async function getPlaylistTracks(token: string, playlistId: string) {
-	type FilteredPlaylistItem = {
-		track: Pick<
-			SpotifyApi.TrackObjectFull,
-			'id' | 'name' | 'is_local' | 'duration_ms' | 'type'
-		> & {
-			album: Pick<SpotifyApi.TrackObjectFull['album'], 'id' | 'name' | 'images'>
-			artists: Pick<SpotifyApi.TrackObjectFull['album']['artists'][number], 'id' | 'name'>[]
+	const items = await getAll<{ track: FilteredTrack | null }>(
+		`playlists/${playlistId}/tracks`,
+		token,
+		{
+			fields: 'total,items.track(album(id,name,artists,images),artists(id,name),id,name,is_local,duration_ms,type)'
 		}
-	}
-	const items = await getAll<FilteredPlaylistItem>(`playlists/${playlistId}/tracks`, token, {
-		fields: 'total,items.track(album(id,name,artists,images),artists(id,name),id,name,is_local,duration_ms,type)'
-	})
-	return items.filter(item => item.track.type == 'track').map(item => item.track)
+	)
+	return items
+		.filter((item): item is { track: FilteredTrack } => item.track?.type == 'track')
+		.map<TrackObj>((item, index) => ({ index, ...item.track }))
 }
 
 export async function removeTracksToPlaylist(token: string, playlistId: string, uris: string[]) {
